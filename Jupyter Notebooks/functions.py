e2a5de41a95d220da24137adfe6a77d78e9e9985
geniomaluco <adrian.arayaaguilar@gmail.com>
2025-07-22 10:40:52 -0600

    "Creacion de archivo de functions.py y Notas.ipynb con ejemplos de uso de pandas y matplotlib.\n\n"

---
+++ Jupyter Notebooks/functions.py
@@ -0,0 +1,8 @@
+def suma(a: list[int]):
+    """
+    Suma los elementos de una lista.
+
+    :param a: Lista de números a sumar.
+    :return: Suma de los números en la lista.
+    """
+    return sum(a)
